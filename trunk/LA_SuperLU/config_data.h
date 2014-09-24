aac48c21200220f3f1a60dcdba3480eaaff01014
Dragan Nikolić <dnikolic@daetools.com>
2014-09-24 15:08:09 +0000

    git-svn-id: svn://svn.code.sf.net/p/daetools/code@535 e944343b-51ee-4bc0-bd0d-d7880edd40ba

---
+++ trunk/LA_SuperLU/config_data.h
@@ -0,0 +1,11 @@
+#include "../config.h"
+
+void get_from_config(bool& bUseUserSuppliedWorkSpace, real_t& dWorkspaceMemoryIncrement, real_t& dWorkspaceSizeMultiplier, std::string& strReuse)
+{
+    daeConfig& cfg = daeConfig::GetConfig();
+    bUseUserSuppliedWorkSpace = cfg.Get<bool>  ("daetools.superlu.useUserSuppliedWorkSpace",    false);
+    dWorkspaceMemoryIncrement = cfg.Get<double>("daetools.superlu.workspaceMemoryIncrement",    1.5);
+    dWorkspaceSizeMultiplier  = cfg.Get<double>("daetools.superlu.workspaceSizeMultiplier",     2.0);
+    strReuse                  = cfg.Get<std::string>("daetools.superlu.factorizationMethod",    std::string("SamePattern"));
+}
+
