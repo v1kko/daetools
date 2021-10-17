0445a4cb956911048bd136a43b9801e8330f22cc
Victor Azizi <victor@lipsum.eu>
2021-10-17 12:15:49 +0200

    Correct imports in test functions

--- daetools-package/daetools/unit_tests/tests_jacobian_expressions.py
+++ daetools-package/daetools/unit_tests/tests_jacobian_expressions.py
@@ -6,7 +6,7 @@ daetools_root = os.path.abspath('../../')
 if not daetools_root in sys.path:
     sys.path.insert(0, daetools_root)
 from daetools.pyDAE import *
-import tests_object
+import daetools.unit_tests.tests_object as tests_object
 
 """
 Tests for:
